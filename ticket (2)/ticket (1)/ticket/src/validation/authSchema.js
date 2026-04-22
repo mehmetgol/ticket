ef9f553e5bc4d7fa1dc7c0a27e4422b9408ac7bb
mehmetgol <golmehmetemre63@gmail.com>
2026-04-22 22:45:08 +0300

    thw

--- ticket (2)/ticket (1)/ticket/src/validation/authSchema.js
+++
@@ -1,6 +0,0 @@
-import * as Yup from 'yup';
-
-export const loginSchema = Yup.object().shape({
-    email: Yup.string().email('Geçersiz email').required('Email zorunlu'),
-    password: Yup.string().min(6, 'Şifre en az 6 karakter olmalı').required('Şifre zorunlu'),
-});
